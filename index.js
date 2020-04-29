b545a3472699498d9188bba205dde54f277239da
Peter Smart <iyiolapeter@gmail.com>
2020-04-29 19:08:16 +0100

    feat(server): added view engine and home page route for todo app

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('This is my index');
+'use strict'
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+// GET - Request resources from server
+// POST - Send data to the server (general purpose)
+// PUT - Send data to the server at a particular location
+// PATCH - Send data to update something on the server
+// OPTIONS - 
+// HEAD - 
+
+
+app.use('/assets', express.static(path.join(__dirname, './assets')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Todo Application'
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.send('This is our login page');
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Listening on Port 3000');
+});
