5e28a88c54df8bbaf47bdaf9a3b16bfadaf5bf24
Peter Smart <iyiolapeter@gmail.com>
2020-05-08 20:44:15 +0100

    refactor(app): moved express app to separate module

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 'use strict'
 
-const path = require('path');
-const express = require('express');
+const { app } = require('./app');
+const { App } = require('./config')
 
-const app = express();
-
-// GET - Request resources from server
-// POST - Send data to the server (general purpose)
-// PUT - Send data to the server at a particular location
-// PATCH - Send data to update something on the server
-// OPTIONS - 
-// HEAD - 
-
-
-app.use('/assets', express.static(path.join(__dirname, './assets')));
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'My Todo Application'
-    });
-});
-
-app.get('/login', (req, res) => {
-    res.send('This is our login page');
-});
-
-
-app.listen(3000, ()=>{
-    console.log('Listening on Port 3000');
+app.listen(App.PORT, ()=>{
+    console.log(`Listening on Port ${App.PORT}`);
 });
